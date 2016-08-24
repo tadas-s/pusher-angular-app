d81aefd75eb6b3d0c3f6255afb72be8bb3119e2d
Tadas Sasnauskas <tadas@yoyo.lt>
2016-08-24 14:47:12 +0100

    Clean app after: yo angular Boop

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name boopApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the boopApp
+ */
+angular.module('boopApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name boopApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the boopApp
+ */
+angular.module('boopApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
